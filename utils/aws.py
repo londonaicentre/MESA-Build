217ff4f1d7b5d1f93e5bed257ff8e3443befa404
Martin Chapman <contact@martinchapman.co.uk>
2025-10-30 18:37:35 +0000

    refactor(datagen): centralise single use AWS logic

--- utils/aws.py
+++ utils/aws.py
@@ -14,25 +14,3 @@ def upload_file(region_name, file_name, bucket, object_name=None, path=None):
         print(e)
         return False
     return True
-
-def start_batch_inference(region_name, job_id, model_id, role_arn, bucket, batch_file):
-    try:
-        boto3.client("bedrock", region_name=region_name).create_model_invocation_job(
-            jobName="genollama-" + job_id.replace("/", "-"),
-            modelId=model_id,
-            roleArn=role_arn,
-            inputDataConfig={
-                "s3InputDataConfig": {
-                    "s3Uri": "s3://" + bucket + "/" + job_id + "/input/" + batch_file
-                }
-            },
-            outputDataConfig={
-                "s3OutputDataConfig": {
-                    "s3Uri": "s3://" + bucket + "/" + job_id + "/output/" 
-                }
-            },
-        )
-    except ClientError as e:
-        print(e)
-        return False
-    return True
