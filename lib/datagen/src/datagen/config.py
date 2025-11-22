78568ce486798b7144cb31ffed6a211130433a32
Martin Chapman <contact@martinchapman.co.uk>
2025-11-22 23:33:45 +0000

    style(datagen): add and apply formatter
    - add ruff as a dependency and to makefile
    - update content based on ruff linter output
    - apply ruff formatter

--- lib/datagen/src/datagen/config.py
+++ lib/datagen/src/datagen/config.py
@@ -4,13 +4,19 @@ from importlib.resources import files
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
 
+
 class ModelConfig(BaseModel):
     model: str
     region: str
     batch_file: str
 
+
 class Config(BaseSettings):
     models: dict[str, ModelConfig]
-    
+
     def __init__(self) -> None:
-        super().__init__(models=json.loads(files("datagen").joinpath("config/config.json").read_text()))
+        super().__init__(
+            models=json.loads(
+                files("datagen").joinpath("config/config.json").read_text()
+            )
+        )
