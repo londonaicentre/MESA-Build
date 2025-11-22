50532d9b0caccc4e026bad236e93cd9c897888bc
Martin Chapman <contact@martinchapman.co.uk>
2025-11-22 23:40:17 +0000

    style(infer/demo): add and apply formatter
    - add ruff as a dependency and to makefile
    - update content based on ruff linter output
    - apply ruff formatter

--- lib/infer/demo/llamadeploy/src/deploy/llama.py
+++ lib/infer/demo/llamadeploy/src/deploy/llama.py
@@ -6,6 +6,7 @@ from sagemaker.djl_inference import DJLModel, DJLPredictor
 
 from deploy.config import Config, ImageConfig
 
+
 def get_image_uri(framework: str, region: str, instance_type: str) -> str | None:
     config: Config = Config()
     if framework not in list(config.images.keys()):
@@ -18,90 +19,93 @@ def get_image_uri(framework: str, region: str, instance_type: str) -> str | None
         base_framework_version=image_arguments.pytorch_version,
         py_version=image_arguments.py_version,
         instance_type=instance_type,
-        image_scope="inference"
+        image_scope="inference",
     )
 
+
 def get_model(model_data: str, role: str, image_uri: str, region: str) -> Model | None:
-    if image_uri and 'huggingface' in image_uri:
+    if image_uri and "huggingface" in image_uri:
         return HuggingFaceModel(
             model_data=model_data,
             role=role,
             image_uri=image_uri,
-            sagemaker_session=sagemaker.Session(boto_session=boto3.Session(region_name=region)),
-            model_server_workers=1
+            sagemaker_session=sagemaker.Session(
+                boto_session=boto3.Session(region_name=region)
+            ),
+            model_server_workers=1,
         )
-    elif image_uri and 'djl' in image_uri:
+    elif image_uri and "djl" in image_uri:
         return DJLModel(
             model_data=model_data,
             role=role,
             image_uri=image_uri,
-            sagemaker_session=sagemaker.Session(boto_session=boto3.Session(region_name=region)),
-            env = {
-                "OPTION_MAX_MODEL_LEN": "117128"
-            }   
+            sagemaker_session=sagemaker.Session(
+                boto_session=boto3.Session(region_name=region)
+            ),
+            env={"OPTION_MAX_MODEL_LEN": "117128"},
         )
     return None
 
-def deploy_demo(model: Model, instance_type: str, endpoint_name: str) -> bool: 
+
+def deploy_demo(model: Model, instance_type: str, endpoint_name: str) -> bool:
     model.deploy(
         initial_instance_count=1,
         instance_type=instance_type,
-        endpoint_name=endpoint_name
+        endpoint_name=endpoint_name,
     )
     if isinstance(model, HuggingFaceModel):
-        return test_predict(HuggingFacePredictor(endpoint_name=endpoint_name)) # type: ignore[no-untyped-call]
+        return test_predict(HuggingFacePredictor(endpoint_name=endpoint_name))  # type: ignore[no-untyped-call]
     elif isinstance(model, DJLModel):
-        return test_predict(DJLPredictor(endpoint_name=endpoint_name)) # type: ignore[no-untyped-call]
+        return test_predict(DJLPredictor(endpoint_name=endpoint_name))  # type: ignore[no-untyped-call]
     return False
+
 
 def test_predict(predictor: Predictor) -> bool:
     try:
-        predictor.predict(
-            {
-                "inputs": "hello world"
-            }
-        )
+        predictor.predict({"inputs": "hello world"})
         return True
     except Exception as e:
         print(e)
         return False
 
+
 def delete_demo(image: str, endpoint_name: str) -> bool:
     predictor: Predictor
     if "huggingface" in image:
-        predictor = HuggingFacePredictor(endpoint_name=endpoint_name) # type: ignore[no-untyped-call]
+        predictor = HuggingFacePredictor(endpoint_name=endpoint_name)  # type: ignore[no-untyped-call]
     elif "djl" in image:
-        predictor = DJLPredictor(endpoint_name=endpoint_name) # type: ignore[no-untyped-call]
+        predictor = DJLPredictor(endpoint_name=endpoint_name)  # type: ignore[no-untyped-call]
     else:
         return False
-    predictor.delete_model() # type: ignore[no-untyped-call]
-    predictor.delete_endpoint() # type: ignore[no-untyped-call]
+    predictor.delete_model()  # type: ignore[no-untyped-call]
+    predictor.delete_endpoint()  # type: ignore[no-untyped-call]
     return True
 
-def run_deploy_up(bucket: str, path: str, sagemaker_execution_role: str, image: str, instance_type: str) -> bool:
+
+def run_deploy_up(
+    bucket: str,
+    path: str,
+    sagemaker_execution_role: str,
+    image: str,
+    instance_type: str,
+) -> bool:
     config: Config = Config()
     image_uri: str | None = get_image_uri(
-        image,
-        config.models["llama"].region,
-        instance_type
+        image, config.models["llama"].region, instance_type
     )
-    if image_uri is None: return False
+    if image_uri is None:
+        return False
     model: Model | None = get_model(
-        "s3://" + bucket + "/" + path + "/model.tar.gz", 
-        sagemaker_execution_role, 
+        "s3://" + bucket + "/" + path + "/model.tar.gz",
+        sagemaker_execution_role,
         image_uri,
-        config.models["llama"].region
+        config.models["llama"].region,
     )
-    if model is None: return False
-    return deploy_demo(
-        model,
-        instance_type,
-        config.models["llama"].endpoint_name
-    )
+    if model is None:
+        return False
+    return deploy_demo(model, instance_type, config.models["llama"].endpoint_name)
+
 
 def run_deploy_down(image: str) -> bool:
     config: Config = Config()
-    return delete_demo(
-        image, 
-        config.models["llama"].endpoint_name
-    )
+    return delete_demo(image, config.models["llama"].endpoint_name)
