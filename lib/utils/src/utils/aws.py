00cabd1eae9b2ffa247a8a20bdd09d5bc131d95f
Martin Chapman <contact@martinchapman.co.uk>
2025-11-23 09:48:55 +0000

    style(utils): apply formatter
    - add ruff as a dependency and to makefile
    - apply ruff formatter

--- lib/utils/src/utils/aws.py
+++ lib/utils/src/utils/aws.py
@@ -1,26 +1,34 @@
-import boto3
 import os
 import random
 import time
 
+import boto3
 from botocore.exceptions import ClientError
 from litellm import completion, RateLimitError, ModelResponse
 
-def upload_file(region_name: str, file_name: str, bucket: str, object_name: str | None = None, path: str | None = None) -> bool:
+
+def upload_file(
+    region_name: str,
+    file_name: str,
+    bucket: str,
+    object_name: str | None = None,
+    path: str | None = None,
+) -> bool:
     if object_name is None:
         object_name = os.path.basename(file_name)
     try:
         boto3.client("s3", region_name=region_name).upload_file(
-            file_name, 
-            bucket, 
-            path + "/" + object_name if path else object_name
+            file_name, bucket, path + "/" + object_name if path else object_name
         )
     except ClientError as e:
         print(e)
         return False
     return True
 
-def bedrock_completion(model_name: str, system_prompt: str, user_prompt: str, bedrock_api_key: str) -> ModelResponse | None:
+
+def bedrock_completion(
+    model_name: str, system_prompt: str, user_prompt: str, bedrock_api_key: str
+) -> ModelResponse | None:
     max_retries: int = 5
     for attempt in range(max_retries + 1):
         try:
@@ -33,7 +41,7 @@ def bedrock_completion(model_name: str, system_prompt: str, user_prompt: str, be
                     {"content": user_prompt, "role": "user"},
                 ],
                 api_key=bedrock_api_key,
-                stream=False
+                stream=False,
             )
         except RateLimitError:
             if attempt == max_retries:
