47995587736d235c9e237aed8eeb5a05ddcfcdba
Martin Chapman <contact@martinchapman.co.uk>
2025-12-05 16:28:19 +0000

    feat: add oncollama schema (#80)
    * feat(assets): add oncollama schema
    
    * feat(assets): add oncollama inference sys prompt
    
    * feat(types): add schema validation functions
    
    - add validate json function to confirm str -> dict
    - add validate schema function to confirm pydantic -> dict
    - add extra type stubs
    
    * refactor(assets): align genollama valid. and super
    
    - override json validation to check all examples with super
    - override schema validation to output schema following check
    - refactor: load schema for system prompt using get source
    
    * refactor(assets): add oncollama prompt + valid.
    
    - override schema validation for extra checks following super
    - add system prompt load for inference
    
    * refactor(assets): update json valid. method sig.
    
    * docs(assets): add remaining docstrings
    
    - add remaining docstrings to oncollama and genollama assets
    - add remaining docstrings to types
    - identify methods that are not yet implemented for partial pipelines

--- lib/types/src/schemallama_types/assets/wrapper.py
+++ lib/types/src/schemallama_types/assets/wrapper.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
 from importlib.resources import files
 from importlib.resources.abc import Traversable
+import json
 from pathlib import Path
 from typing import Any, cast
+
+from jsonschema import ValidationError
+from pydantic import BaseModel
 
 from schemallama_types.assets.profile import Profile
 from schemallama_types.assets.sampling import Content, Style
@@ -15,8 +19,65 @@ class SchemaLlamaAssets(ABC):
     def _load(self, folder: str, file: str) -> str:
         return self._base_dir.joinpath(f"{folder}/{file}").read_text()
 
+    # schema
+    def validate_json(
+        self, json_str: str, schema: type[BaseModel]
+    ) -> tuple[bool, str, dict[str, Any] | None]:
+        """Validate a schema json string (str -> dict)
+
+        Args:
+            json_str (str): The json string to validate
+            schema: (type[BaseModel]): The schema to validate against
+
+        Returns:
+            tuple: The validation result, result description,
+                and parsed string
+
+        """
+        try:
+            parsed: dict[str, Any] = json.loads(json_str)
+            schema(**parsed)
+            return True, "Valid", parsed
+        except json.JSONDecodeError as e:
+            return False, f"JSON Parse Error: {e}", None
+        except ValidationError as e:
+            return False, f"Schema Validation Error: {e}", None
+        except Exception as e:
+            return False, f"Validation Error: {e}", None
+
+    def validate_schema(
+        self, schema: type[BaseModel]
+    ) -> tuple[bool, str, dict[str, Any] | None]:
+        """Validate a schema (pydantic -> dict)
+
+        Args:
+            schema: (type[BaseModel]): The schema to validate
+
+        Returns:
+            tuple: The validation result, result description,
+                and json version of the schema
+
+        """
+        try:
+            json_schema: dict[str, Any] = schema.model_json_schema()
+            return True, "Schema validation successful", json_schema
+        except Exception as e:
+            return False, f"Schema validation failed: {e}", None
+
     # prompts
     def load_user_prompt_template(self, template_name: str) -> str:
+        """Load a user prompt template from wrapped assets.
+                Template is assumed to be stored in the form
+                `userprompt_{template}.md`
+
+        Args:
+            template_name (str): The name of the user prompt template
+                to load
+
+        Returns:
+            str: The text of the template as a string
+
+        """
         return self._load("prompts", f"userprompt_{template_name}.md")
 
     @abstractmethod
@@ -32,15 +93,13 @@ class SchemaLlamaAssets(ABC):
         pass
 
     # profiles
-    @abstractmethod
-    def _load_profiles_from_file(self, file_path: Traversable) -> list[Profile]:
-        pass
-
-    @abstractmethod
-    def format_profile_prompt(self, profile: Profile) -> str:
-        pass
-
     def load_all_profiles(self) -> list[Profile]:
+        """Load all profiles
+
+        Returns:
+            list: Loaded profiles
+
+        """
         all_profiles: list[Profile] = []
         items: list[Traversable] = cast(
             list[Traversable], sorted(self._base_dir.joinpath("profiles").iterdir())
@@ -53,6 +112,12 @@ class SchemaLlamaAssets(ABC):
         return all_profiles
 
     def load_profiles_from_files(self, filenames: list[str]) -> list[Profile]:
+        """Load profiles by file name
+
+        Returns:
+            list: Loaded profiles
+
+        """
         all_profiles: list[Profile] = []
         for filename in filenames:
             file_path: str = "profiles/" + filename
@@ -65,15 +130,45 @@ class SchemaLlamaAssets(ABC):
                 raise FileNotFoundError(f"Profile file not found: {file_path}")
         return all_profiles
 
+    @abstractmethod
+    def _load_profiles_from_file(self, file_path: Traversable) -> list[Profile]:
+        pass
+
+    @abstractmethod
+    def format_profile_prompt(self, profile: Profile) -> str:
+        pass
+
     # styles
     def load_style_data(self) -> Style:
+        """Load data from style file into Style class
+
+        Returns:
+            Style: Loaded style data
+
+        """
         return Style(self._base_dir.joinpath("style.yml"))
 
     def load_content_data(self) -> Content:
+        """Load data from content file into Content class
+
+        Returns:
+            Content: Loaded content data
+
+        """
         return Content(self._base_dir.joinpath("content.yml"))
 
     # structures
     def load_structures(self, enabled_structures: list[str]) -> dict[str, str]:
+        """Load structures
+
+        Args:
+            enabled_structures (list): Specified files containing example
+                structures to include when building a prompt
+
+        Returns:
+            dict: A mapping between structure file names and content
+
+        """
         self.structures: dict[str, str] = {}
         for filename in enabled_structures:
             file_path: str = "structure/" + filename
@@ -86,4 +181,13 @@ class SchemaLlamaAssets(ABC):
         return self.structures
 
     def get_structure_name_without_extension(self, filename: str) -> str:
+        """Return a copy of a structure filename without its extension
+
+        Args:
+            filename (str): Filename with extension
+
+        Returns:
+            str: Structure filename
+
+        """
         return Path(self._base_dir.joinpath("structure/" + filename).name).stem
