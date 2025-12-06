256ec3e12a751daeacc896bb6eb559d1c788d421
Martin Chapman <contact@martinchapman.co.uk>
2025-12-06 13:28:02 +0000

    chore(oncollama): pass setting to docsynth
    - add settings wrapper for env vars
    - pass batch credentials to docsynth generator
    - add .jsonl to ignore

--- pipelines/oncollama/src/oncollama/docsynth.py
+++ pipelines/oncollama/src/oncollama/docsynth.py
@@ -1,6 +1,10 @@
+from oncollama.settings import Settings
 from oncollama_assets.wrapper import OncoLlamaAssets
 from docsynth.generate import Generator
 
 
 def main() -> None:
-    Generator().generate(OncoLlamaAssets())
+    settings: Settings = Settings()
+    Generator().generate(
+        OncoLlamaAssets(), settings.US_BUCKET, settings.BEDROCK_EXECUTION_ROLE
+    )
