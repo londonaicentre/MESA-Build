b65e68c55c7f612772ffe415d9482ace3451e695
Martin Chapman <contact@martinchapman.co.uk>
2025-12-06 13:58:34 +0000

    feat(oncollama): add batch cli arg

--- pipelines/oncollama/src/oncollama/docsynth.py
+++ pipelines/oncollama/src/oncollama/docsynth.py
@@ -1,10 +1,34 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass
+
 from oncollama.settings import Settings
 from oncollama_assets.wrapper import OncoLlamaAssets
 from docsynth.generate import Generator
 
 
-def main() -> None:
-    settings: Settings = Settings()
-    Generator().generate(
-        OncoLlamaAssets(), settings.US_BUCKET, settings.BEDROCK_EXECUTION_ROLE
+@dataclass
+class DocsynthArgs:
+    batch: bool
+
+
+def parse_CLI_args() -> DocsynthArgs:
+    """Parse command line arguments"""
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument(
+        "-b",
+        "--batch",
+        action="store_true",
+        help="Whether to process sample generation request as a batch job. Requires AWS credentials.",
     )
+    return DocsynthArgs(**vars(parser.parse_args()))
+
+
+def main() -> None:
+    args: DocsynthArgs = parse_CLI_args()
+    settings: Settings = Settings()
+    if args.batch:
+        Generator().generate(
+            OncoLlamaAssets(), settings.US_BUCKET, settings.BEDROCK_EXECUTION_ROLE
+        )
+    else:
+        Generator().generate(OncoLlamaAssets())
