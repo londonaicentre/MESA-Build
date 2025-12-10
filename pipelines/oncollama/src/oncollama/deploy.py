e251eeae9acbae253c03f4bf1cb9d7c4ca7f42b8
Martin Chapman <contact@martinchapman.co.uk>
2025-12-10 16:41:11 +0000

    feat(oncollama): add remaining pipeline steps (#94)
    * feat(oncollama): add remaining package imports
    
    * feat(oncollama): add additional settings
    
    * feat(oncollama): add deploy and fine-tune scripts
    
    * chore(oncollama): ignore fine-tune outputs
    
    * docs(oncollama): document remaining pipeline steps
    
    - document additional env vars
    - update section naming
    
    * ci(oncollama): add linter and type checker

---
+++ pipelines/oncollama/src/oncollama/deploy.py
@@ -0,0 +1,39 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass
+
+from deploy.llama import Deployer
+from oncollama.settings import Settings
+
+
+@dataclass
+class DeployArgs:
+    path: str
+    command: str
+
+
+def parse_CLI_args() -> DeployArgs:
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        required=True,
+        help="Path within S3 bucket to the zipped weights of the model to deploy",
+    )
+    parser.add_argument("command", choices=["up", "down"])
+    return DeployArgs(**vars(parser.parse_args()))
+
+
+def main() -> None:
+    args: DeployArgs = parse_CLI_args()
+    settings: Settings = Settings()
+    deployer: Deployer = Deployer(settings.IMAGE, settings.INSTANCE_TYPE)
+
+    if args.command == "up":
+        deployer.run_deploy_up(
+            settings.BUCKET,
+            args.path,
+            settings.SAGEMAKER_EXECUTION_ROLE,
+        )
+    elif args.command == "down":
+        deployer.run_deploy_down()
