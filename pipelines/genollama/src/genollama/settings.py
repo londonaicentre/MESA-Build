4b901c365ca056e71a6770272085f6b10fcd0249
Martin Chapman <contact@martinchapman.co.uk>
2025-12-13 17:03:15 +0000

    refactor(genollama): remove us bucket as setting

--- pipelines/genollama/src/genollama/settings.py
+++ pipelines/genollama/src/genollama/settings.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     BEDROCK_API_KEY: str = ""
     BEDROCK_EXECUTION_ROLE: str = ""
     BUCKET: str = ""
-    US_BUCKET: str = ""
     SAGEMAKER_EXECUTION_ROLE: str = ""
     INSTANCE_TYPE: str = "ml.g5.xlarge"
     IMAGE: str = "djl-lmi"
