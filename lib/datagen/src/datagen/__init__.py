d3a5eb3693dc06a5625976a77b5d58ed7daae6a2
drjzhn <drjzhn@gmail.com>
2026-01-09 12:27:25 +0000

    refactor: document loading from S3 bucket to local (replacing bootstrapper + csv); document data class

--- lib/datagen/src/datagen/__init__.py
+++ lib/datagen/src/datagen/__init__.py
@@ -1,3 +1,9 @@
 from datagen.claude import SampleGenerator
+from datagen.document_loader import DocumentBatchLoader
+from mesa_types import Document
 
-__all__ = ["SampleGenerator"]
+__all__ = [
+    "SampleGenerator",
+    "Document",
+    "DocumentBatchLoader",
+]
