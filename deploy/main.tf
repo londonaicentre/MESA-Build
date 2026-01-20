e82cace9b57fb061798963fccc408fab2860f3ef
Martin Chapman <contact@martinchapman.co.uk>
2026-01-20 22:52:11 +0000

    feat: add versioning to mesa build bucket
    - add prevent destroy
    - add cost-saving settings (limited days and versions)

--- deploy/main.tf
+++ deploy/main.tf
@@ -31,6 +31,31 @@ provider "aws" {
 
 resource "aws_s3_bucket" "mesa_build_bucket" {
   bucket = "aicentre-nlpteam-mesa-build"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "mesa_build_bucket_versioning" {
+  bucket = aws_s3_bucket.mesa_build_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "terraform_state_bucket_lifecycle_configuration" {
+  bucket = aws_s3_bucket.mesa_build_bucket.id
+
+  rule {
+    id = "remove_old_versions"
+    noncurrent_version_expiration {
+      newer_noncurrent_versions = 2
+      noncurrent_days           = 1
+    }
+
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_object" "mesa_build_bucket_documents" {
