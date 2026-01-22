fd269e6b499482784a579f3636b67f7315668e9c
Martin Chapman <contact@martinchapman.co.uk>
2026-01-22 09:35:40 +0000

    chore: delete deprecated files (#141)

--- lib/_deprecate/docsynth/src/docsynth/types/documents.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class DocsynthDocument(BaseModel):
-    doc_id: str
-    doc_name: str
-    prompt: str
-    content: str | None = None
--- lib/_deprecate/infer/demo/deploy/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "keypair" {
-  type = string
-}
-
-variable "litellm_master_key" {
-  type = string
-}
