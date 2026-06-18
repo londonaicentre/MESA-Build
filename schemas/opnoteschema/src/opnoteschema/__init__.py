1998dfb5abec40cd81dc25d5cec621204b7df595
Martin Chapman <contact@martinchapman.co.uk>
2026-06-18 13:44:05 +0100

    refactor: apply templater to opnoteschema

---
+++ schemas/opnoteschema/src/opnoteschema/__init__.py
@@ -0,0 +1,4 @@
+from .schema import OperationNote
+
+# export OperationNote for external use under `Schema` name
+Schema = OperationNote
---
+++ schemas/opnoteschema/tests/test_opnoteschema.py
@@ -0,0 +1,13 @@
+"""Tests for opnoteschema package."""
+
+
+def test_validate_schema() -> None:
+    pass
+
+
+def test_build_datagen_prompt() -> None:
+    pass
+
+
+def test_build_main_prompt() -> None:
+    pass
